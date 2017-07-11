cd47b9490eaebb72321c9d29e166b4ede930e8d5
yangzonglong <yangzonglong@yangzonglongdeMacBook-Pro.local>
2017-07-11 10:01:30 +0800

    新增css loader

--- webpack-config/loaders-config.js
+++ webpack-config/loaders-config.js
@@ -1,18 +1,27 @@
 const extractTextPlugin = require('extract-text-webpack-plugin');
 module.exports = [{
-    test: /\.js$/,
-    exclude: /node_modules/,
-    loader: 'babel-loader'
-}, {
-    test: /\.ejs$/,
-    exclude: /node_modules/,
-    loader: 'ejs-loader'
-},
-{
-    test: /\.scss$/,
-    exclude: /node_modules/,
-    loader: extractTextPlugin.extract({
-        fallback: 'style-loader',
-        use: ['css-loader', 'sass-loader']
-    })
-}]
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+    }, {
+        test: /\.ejs$/,
+        exclude: /node_modules/,
+        loader: 'ejs-loader'
+    },
+    {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        loader: extractTextPlugin.extract({
+            fallback: 'style-loader',
+            use: ['css-loader']
+        })
+    },
+    {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        loader: extractTextPlugin.extract({
+            fallback: 'style-loader',
+            use: ['css-loader', 'sass-loader']
+        })
+    }
+]
