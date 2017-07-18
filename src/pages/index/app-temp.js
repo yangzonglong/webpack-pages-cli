2b1114954e5fe07cad847a87516342bfa4fd8717
yangzonglong <yangzonglong@yangzonglongdeMacBook-Pro.local>
2017-07-18 16:24:24 +0800

    增加css background loading

--- src/pages/index/app-temp.js
+++ src/pages/index/app-temp.js
@@ -1,3 +1,6 @@
+// 模版入口 在这里组织模版
+// 这个文件必须存在,不然无法生成html页面
+
 import Html from 'Html';
 import header from 'header-temp';
 import footer from 'footer-temp';
@@ -7,6 +10,3 @@ const html = new Html();
 html.temp({header,footer});
 html.data({title:'title'});
 export default html.rend(ejs)
-
-// 模版入口 在这里组织模版
-// 这个文件必须存在,不然无法生成html页面
--- webpack-config/loaders-config.js
+++ webpack-config/loaders-config.js
@@ -1,27 +1,32 @@
 const extractTextPlugin = require('extract-text-webpack-plugin');
 module.exports = [{
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader'
-    }, {
-        test: /\.ejs$/,
-        exclude: /node_modules/,
-        loader: 'ejs-loader'
-    },
-    {
-        test: /\.css$/,
-        exclude: /node_modules/,
-        loader: extractTextPlugin.extract({
-            fallback: 'style-loader',
-            use: ['css-loader']
-        })
-    },
-    {
-        test: /\.scss$/,
-        exclude: /node_modules/,
-        loader: extractTextPlugin.extract({
-            fallback: 'style-loader',
-            use: ['css-loader', 'sass-loader']
-        })
-    }
+    test: /\.js$/,
+    exclude: /node_modules/,
+    loader: 'babel-loader'
+}, {
+    test: /\.ejs$/,
+    exclude: /node_modules/,
+    loader: 'ejs-loader'
+},
+{
+    test: /\.css$/,
+    exclude: /node_modules/,
+    loader: extractTextPlugin.extract({
+        fallback: 'style-loader',
+        use: ['css-loader']
+    })
+},
+{
+    test: /\.(png|jpg|gif)$/,
+    exclude: /node_modules/,
+    loader: 'url-loader?limit=10240'
+},
+{
+    test: /\.scss$/,
+    exclude: /node_modules/,
+    loader: extractTextPlugin.extract({
+        fallback: 'style-loader',
+        use: ['css-loader', 'sass-loader']
+    })
+}
 ]
